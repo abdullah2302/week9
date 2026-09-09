5661284e3d112807269f3667a7ce951aeca58a2b
HP <abdullahbhatti8776@gmail.com>
2026-09-09 10:11:52 +0500

    fix: adjust Footer layout for improved spacing and structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,8 +12,10 @@ import {
 
 function Footer() {
     return (
+
+        <>
         <footer className="border-t border-slate-800 bg-slate-950 text-slate-400 text-center">
-            <div className="mx-auto grid max-w-6xl gap-10 px-4 py-14 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="mx-auto grid max-w-6xl grid-cols-4 gap-6 px-4 py-14 sm:gap-10 sm:grid-cols-2 lg:grid-cols-4">
                 <div>
                     <h3 className="mb-3 flex items-center text-2xl font-bold text-white justify-center" >
                         <FontAwesomeIcon 
@@ -110,6 +112,7 @@ function Footer() {
                 © {new Date().getFullYear()} Shoply. All rights reserved.
             </div>
         </footer>
+        </>
     );
 }
 
